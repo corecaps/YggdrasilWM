78b0b94b5806f95add2325bb055fb98816ce1492
corecaps <corecaps@gmail.com>
2024-01-31 03:08:45 +0100

    Harmonizing comments header

--- inc/EventHandler.hpp
+++ inc/EventHandler.hpp
@@ -1,4 +1,12 @@
 /**
+ * Yb  dP              8                w 8 Yb        dP 8b   d8
+ *  YbdP  .d88 .d88 .d88 8d8b .d88 d88b w 8  Yb  db  dP  8YbmdP8
+ *   YP   8  8 8  8 8  8 8P   8  8 `Yb. 8 8   YbdPYbdP   8  "  8
+ *   88   `Y88 `Y88 `Y88 8    `Y88 Y88P 8 8    YP  YP    8     8
+ *        wwdP wwdP
+ * Yggdrasil Window Manager
+ * by jgarcia <jgarcia@student.42.fr> <corecaps@gmail.com>
+ * https://github.com/corecaps/YggdrasilWM
  * @file EventHandler.hpp
  * @brief EventHandler class header.
  * @date 2021-06-23
--- src/TreeLayoutManager.cpp
+++ src/TreeLayoutManager.cpp
@@ -1,7 +1,18 @@
-//
-// Created by corecaps on 1/12/24.
-//
+/**
+ * Yb  dP              8                w 8 Yb        dP 8b   d8
+ *  YbdP  .d88 .d88 .d88 8d8b .d88 d88b w 8  Yb  db  dP  8YbmdP8
+ *   YP   8  8 8  8 8  8 8P   8  8 `Yb. 8 8   YbdPYbdP   8  "  8
+ *   88   `Y88 `Y88 `Y88 8    `Y88 Y88P 8 8    YP  YP    8     8
+ *        wwdP wwdP
+ * Yggdrasil Window Manager
+ * by jgarcia <jgarcia@student.42.fr> <corecaps@gmail.com>
+ * https://github.com/corecaps/YggdrasilWM
+ * @file TreeLayoutManager
+ * @brief handle Tree Style Layout.
+ * @date 2021-06-23
+ */
 
+#include <iostream>
 #include "TreeLayoutManager.hpp"
 
 TreeLayoutManager::TreeLayoutManager(Display *display, Window root) : LayoutManager(display, root) {
