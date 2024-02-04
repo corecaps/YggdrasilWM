e91fe3d910182267338cef7a0fcf52fbc0dd376c
corecaps <corecaps@gmail.com>
2024-02-04 13:14:25 +0100

    little script to help with documenting

---
+++ update_date.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the current date in the desired format (YYYY-MM-DD)
+current_date=$(date +'%Y-%m-%d')
+
+# List files in specified directories
+files=$(find src/ inc/ test/ -type f)
+
+# Loop through each file
+for file in $files; do
+    # Check if the last modification date is today
+    last_modified_date=$(date -r "$file" +'%Y-%m-%d')
+    if [ "$last_modified_date" == "$current_date" ]; then
+        # Update the @date line in the file
+        sed -i "s/@date.*/@date $current_date/" "$file"
+        echo "Updated @date in $file"
+    fi
+  done
